1ec87366a7eb79d595abac5d536a20c5ff2cc4af
Tanktwo <rhkdtjr900@gmail.com>
2020-11-13 20:56:44 +0900

    [cnp004] 새로 갈아엎고 다시처음부터 - StartButton 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,7 @@
-import React, { useEffect, useState, createContext } from 'react';
-
-import Container from './container';
-import Start from './start';
-
-export const AppContext = createContext({});
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const setFlag = (e: number) => {
-    setState((prevState) => {
-      return { ...prevState, flag: e };
-    });
-  };
-  const plusOne = () => {
-    setState((prevState) => {
-      return { ...prevState, score: prevState.score + 1 };
-    });
-    setFlag(0);
-  };
-  const scoreZero = () => {
-    setState((prevState) => {
-      return { ...prevState, score: 0 };
-    });
-    setFlag(0);
-  };
-  const scoreMinusTwo = () => {
-    setState((prevState) => {
-      return { ...prevState, score: prevState.score-2 };
-    });
-    setFlag(0);
-  };
-  const setUiStatus = (e: 'init' | 'ing') => {
-    setState((prevState) => {
-      return { ...prevState, uiStatus: e };
-    });
-    setFlag(0);
-  };
-
-  const initState = {
-    score: 0,
-    plusOne,
-    scoreZero,
-    scoreMinusTwo,
-    flag: 0,
-    setFlag,
-    uiStatus: 'init',
-    setUiStatus,
-  };
-  const [state, setState] = useState(initState);
-
-  useEffect(() => {
-    if (state.score > 10) {
-      alert('10점 !');
-      setUiStatus('init');
-    }
-    // eslint-disable-next-line
-  }, [state.score]);
-
-  return (
-    <AppContext.Provider value={state}>
-      <div
-        className="App"
-        style={
-          state.uiStatus === 'init'
-            ? { background: 'black', height: '100vh', overflow: 'hidden' }
-            : {}
-        }
-      >
-        {state.uiStatus === 'init' ? <Start /> : <Container />}
-      </div>
-    </AppContext.Provider>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
